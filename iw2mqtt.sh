ab2aa91caed26084fe011f899aeda35c8329e23e
Mike Abrahall <mike@abrahall.id.au>
2023-08-08 15:14:04 +1000

    Fixed all the things

--- iw2mqtt.sh
+++ iw2mqtt.sh
@@ -1,13 +1,22 @@
 #!/bin/ash
 
+HOSTNAME="$(uci show system.@system[0].hostname)"
+HOSTNAME="${HOSTNAME#*=\'}"
+HOSTNAME="${HOSTNAME%\'}"
+
 # FIXME: This should be unique to each AP
-AVAILABILITY_TOPIC="mijofa-iw2mqtt/availability/mike.abrahall.id.au"
-STATE_TOPIC="mijofa-iw2mqtt/state/mike.abrahall.id.au"
-# FIXME: This should be configurable
-ZONE_NAME=${ZONE_NAME:-home}
-# FIXME: Grab MQTT_USER/PW/HOST from config as well
+AVAILABILITY_TOPIC="mijofa-iw2mqtt/availability/$HOSTNAME"
+STATE_TOPIC="mijofa-iw2mqtt/state/$HOSTNAME"
+ATTRS_TOPIC="mijofa-iw2mqtt/attributes/$HOSTNAME"
+# FIXME: Grab LOCATION & MQTT_USER/PW/HOST from config as well
+
+# use payload_reset if location is set, otherwise payload_home
+home_or_reset="${LOCATION:+reset}"
+home_or_reset="${home_or_reset:-home}"
 
 # FIXME: This should somehow include other AP's unique availability topics
+# FIXME: source_type should be 'router' not 'gps', but routers don't seem to be able to specify any zone other than "home"
+# FIXME: The configuration_url should be something like a Luci URL, not the upstream source/documentation
 discovery_template='{"device":{
                          "configuration_url":"https://github.com/mijofa/iw2mqtt",
                          "manufacturer":"mijofa",
@@ -15,39 +24,37 @@ discovery_template='{"device":{
                          "name":"OpenWRT WiFi Devices",
                          "identifiers":["mijofa-iw2mqtt"]
                     },
-                    "source_type":"#SOURCETYPE#",
+                    "source_type":"gps",
                     "icon":"mdi:router-wireless",
-                    "availability":[{"topic":"'"$AVAILABILITY_TOPIC"'"}],
+                    "availability":[{"topic":"'"$AVAILABILITY_TOPIC"'",
+                                     "value_template":"{{ True if (value_json is not boolean) else value_json }}",
+                                     "payload_available":true,
+                                     "payload_not_available":false
+                    }],
 
                     "state_topic":"'"$STATE_TOPIC"'",
-                    "value_template":"{{\"#MAC#\" in value_json.connected_devices or \"#MAC#\" in value_json.previous_devices}}",
-                    "value_home":true,
-                    "value_not_home":false,
+                    "value_template":"{{ \"#MAC#\" in value_json.connected_devices + value_json.previous_devices }}",
+                    "payload_'"$home_or_reset"'":true,
+                    "payload_not_home":false,
+
+                    "json_attributes_topic":"'"$AVAILABILITY_TOPIC"'",
+
                     "unique_id":"mijofa-iw2mqtt.#ID#",
                     "object_id":"mijofa-iw2mqtt.#ID#",
                     "name":"#MAC#"}'
-# FIXME: source_type should be 'router' not 'gps', but routers don't seem to be able to specify any zone other than "home"
-if [[ "$ZONE_NAME" == "home" ]] ; then
-    discovery_template=${discovery_template/#SOURCETYPE#/router}
-else
-    echo "ERROR: Sorry, non 'home' zone names are not currently supported."
-    exit 2
-    discovery_template=${discovery_template/#SOURCETYPE#/gps}
-fi
 
 if [[ "$1" != "--verbose" ]] ; then
-    VERBOSE="no"
+    LOG_MQTT="no"
 fi
-maybe_log() {
-    test "$VERBOSE" == "no" || printf '%s\n' "$@" >&2
-}
 
 mqtt_pub() {
     topic=$1
     shift
     message=$1
     shift
-    maybe_log "MQTT: $topic \"$message\""
+    # Would be nice to use '%q' here, but BusyBox/OpenWRT's printf doesn't support that
+    test "$LOG_MQTT" == "no" || printf '%s: mqtt_pub(%s): %s\n' "$(date -Iseconds)" "$topic" "$message" >&2
+    # test "$LOG_MQTT" == "no" || echo "$message" | sed "s|^|$(date -Iseconds) mqtt_pub($topic) |" >&2
 
     mosquitto_pub --username $MQTT_USER --pw $MQTT_PW --host $MQTT_HOST --topic "$topic" --message "$message" "$@"
 }
@@ -74,9 +81,22 @@ configure_discovery_for_mac() {
 }
 
 update_connections() {
-    # We keep the previous list around so that HA can do some delayed device expiry for intermittent WiFi outages
+    # We keep the previous list around for some delayed device expiry for intermittent WiFi outages.
+    # This is implemented by having the value_template simply check both lists
     previous_devices_list="${connected_devices_list:-[]}"
     connected_devices_list="$(list_connected_MACs | sed 's/^/ "/;1s/^ /[/;s/$/",/;$s/,$/]/')"
+    mqtt_pub "$STATE_TOPIC" "{\"last_update\":$(date +'%s'),
+                              \"connected_devices\":$connected_devices_list,
+                              \"previous_devices\":$previous_devices_list}"
+}
+
+new_connection() {
+    configure_discovery_for_mac "$1"
+
+    # We don't want to update the expiry of old previous connections when a single new connections arrives.
+    # So just resend the old data, with this 1 MAC address appended,
+    # since we also don't care if there's duplicate entries in the list.
+    connected_devices_list="${connected_devices_list%]},\"$1\"]"
     mqtt_pub "$STATE_TOPIC" "{\"last_update\":$(date +'%s'),
                               \"connected_devices\":$connected_devices_list,
                               \"previous_devices\":$previous_devices_list}"
@@ -87,63 +107,36 @@ update_connections() {
 # rather than letting it continue to trust the outdated info.
 # FIXME: Why OpenWRT doesn't have '/dev/fd' by default? OpenWRT's ash requires it for this?
 test -L /dev/fd || ln -s /proc/self/fd /dev/fd
-exec 3> >(exec mosquitto_pub --username $MQTT_USER --pw $MQTT_PW --host $MQTT_HOST --will-topic "$AVAILABILITY_TOPIC" --will-payload "offline" --topic "$AVAILABILITY_TOPIC" --stdin-line)
-echo >&3 "online"
+exec 3> >(exec mosquitto_pub --username $MQTT_USER --pw $MQTT_PW --host $MQTT_HOST --will-topic "$AVAILABILITY_TOPIC" --will-payload "false" --will-retain --topic "$AVAILABILITY_TOPIC" --stdin-line --retain)
 
+# Pre-load the currently connected devices before we start listening for new devices
 list_connected_MACs | while read mac ; do
     configure_discovery_for_mac "$mac"
 done
 configure_discovery_for_mac "5e:0c:a4:36:ae:81"
 update_connections
 
+# Tell HA that the current MQTT data is valid, and it can mark the entities as available now.
+echo "${LOCATION:-true}" >&3
+
 {
     # FIXME: The only reason for this interval is so that we can update disconnects only a minute later.
-    #        How can we do this better?
+    #        Should we just run the 'update_connections' function in it's own background loop?
     while sleep 6 ; do
         printf '[%s]: n/a: ping station ALL\n' "$(date +'%Y-%m-%d %T.000000')"
     done &
 
     iw event -T
 } | while read date time nic event station mac ; do
-    echo >&3 "online"
-
     # Ignore the "unknown event" lines we get from `iw event`
     [[ "$station" == "station" ]] || continue
 
-    # Do discovery for new connections (not disconnects)
-    [[ "$event" == "new" ]] && configure_discovery_for_mac "$mac"
+    # Ignore disconnect events, because it might be a transient outage, so just let them expire out instead
+    [[ "$event" == "del" ]] && continue
 
     # Update active connections with new connections and every interval
-    update_connections
-done
+    [[ "$event" == "ping" ]] && update_connections
 
-# Discovery info example/template
-# NOTE: By ensuring all "device" dicts are identical, we can have all the entities grouped in the same device
-# ref: https://www.home-assistant.io/integrations/device_tracker.mqtt/#using-the-discovery-protocol
-# topic: homeassistant/device_tracker/mijofa-iw2mqtt/#ID#/config
-# { "device": {
-#     "configuration_url": "https://github.com/mijofa/iw2mqtt",
-#     "manufacturer": "mijofa",
-#     "model": "iw2mqtt",
-#     "name": "OpenWRT WiFi Devices",
-#     "identifiers": ["mijofa-iw2mqtt"]
-#     # FIXME: This would need to be identical for all trackers in order to combine them properly.
-#     #        Is this even supposed to have these MACs?
-#     # "connections": [
-#     #     ["mac", "2a:e0:9b:0d:0e:1e"],
-#     #     ["mac", "5e:0c:a4:36:ae:81"]
-#     # ],
-#   },
-#   "source_type": "router",
-#   "icon": "mdi:router-wireless",
-#   "availability": [
-#     # This is unique to each AP so we can set a will-topic & will-payload properly.
-#     # However every AP needs to know about each other for this to work properly.
-#     {"topic": "mijofa-iw2mqtt/availability/mike.abrahall.id.au"}
-#   ],
-#   # Entity specific
-#   "state_topic": "mijofa-iw2mqtt/tracker/#ID#",
-#   "unique_id": "mijofa-iw2mqtt.#ID#",
-#   "object_id": "mijofa-iw2mqtt.#ID#",
-#   "name": "#MAC#"
-# }
+    # Update new connections immediately instead of waiting for the ping
+    [[ "$event" == "new" ]] && new_connection "$mac"
+done
